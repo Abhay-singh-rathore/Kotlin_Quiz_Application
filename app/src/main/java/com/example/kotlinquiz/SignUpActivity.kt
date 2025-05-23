0dfc1dfbbb4c8239e72a3cfeb3108a2656352e62
abhay <abhaysinghrathore756@gmail.com>
2025-05-24 00:29:33 +0530

    SignUp page with backend and authorized by Firebase auth and connected to firebase

--- app/src/main/java/com/example/kotlinquiz/SignUpActivity.kt
+++ app/src/main/java/com/example/kotlinquiz/SignUpActivity.kt
@@ -1,20 +1,56 @@
 package com.example.kotlinquiz
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.kotlinquiz.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+    lateinit var signUpBinding: ActivitySignUpBinding
+
+    val auth : FirebaseAuth=FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        signUpBinding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view =signUpBinding.root
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(view)
+
+
+        signUpBinding.buttonSignUp.setOnClickListener {
+            val email = signUpBinding.edittextSignupEmail.text.toString()
+            val password = signUpBinding.edittextSignUppassword.text.toString()
+
+            signUpWithFirebase(email,password)
+
         }
+
     }
+
+    fun signUpWithFirebase(email : String, password :String){
+        signUpBinding.progressBarSignUp.visibility = View.VISIBLE
+        signUpBinding.buttonSignUp.isClickable = false
+
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                Toast.makeText(applicationContext,"Your account has been created",Toast.LENGTH_SHORT).show()
+                finish()
+                signUpBinding.progressBarSignUp.visibility =View.INVISIBLE
+                signUpBinding.buttonSignUp.isClickable = true
+            } else{
+
+                Toast.makeText(applicationContext,task.exception?.localizedMessage,Toast.LENGTH_SHORT).show()
+
+            }
+
+            }
+
+
+    }
+
 }
