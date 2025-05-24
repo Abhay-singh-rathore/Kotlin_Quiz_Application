a4089909d5b9193b2fa9c1f2af65d27889bbad27
abhay <abhaysinghrathore756@gmail.com>
2025-05-24 12:15:02 +0530

    Complete Question Page Design

--- app/src/main/java/com/example/kotlinquiz/QuizActivity.kt
+++ app/src/main/java/com/example/kotlinquiz/QuizActivity.kt
@@ -5,16 +5,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.kotlinquiz.databinding.ActivityQuizBinding
 
 class QuizActivity : AppCompatActivity() {
+    lateinit var quizBinding : ActivityQuizBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        quizBinding=ActivityQuizBinding.inflate(layoutInflater)
+        val view = quizBinding.root
         enableEdgeToEdge()
-        setContentView(R.layout.activity_quiz)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(view)
+
+        quizBinding.buttonFinish.setOnClickListener {
+
         }
+
+        quizBinding.buttonNext.setOnClickListener {
+
+        }
+
+        quizBinding.textViewA.setOnClickListener {  }
+
+        quizBinding.textViewB.setOnClickListener {  }
+
+        quizBinding.textViewC.setOnClickListener {  }
+
+        quizBinding.textViewD.setOnClickListener {  }
     }
 }
